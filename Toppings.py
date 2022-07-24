8e278a3975aad9403099a59332c74b5edbe61c62
Aaron <aaron.lin.2@stonybrook.edu>
2022-07-23 20:44:44 -0400

    Deleted unused files

--- Toppings.py
+++
@@ -1,5 +0,0 @@
-class Toppings:
-    def __init__(self, topping1, topping2) -> None:
-        self._topping1 = topping1
-        self._topping2 = topping2
-
--- ice_cream.py
+++
@@ -1,6 +0,0 @@
-class IceCream:
-    def __init__(self, iceCreamFlavor) -> None:
-        self.iceCream = iceCreamFlavor
-
-class IceCreamNode:
-    
