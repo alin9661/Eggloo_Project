a36a7c8e0e47f30e5ce35cf343b9576d0d1378ff
Aaron <aaron.lin.2@stonybrook.edu>
2022-08-11 16:08:48 -0400

    deleted unused code

--- project.py
+++ project.py
@@ -4,15 +4,6 @@ import itertools
 import random
 import os
 
-def listRandomizer(listOfVideos):
-    random.shuffle(listOfVideos)
-
-def listOfVideoClips(folder_name):
-    listOfFiles = os.listdir(folder_name)
-    pathName = os.path.abspath(folder_name)
-    listRandomizer(listOfFiles)
-    return [VideoFileClip(os.path.join(pathName, video)) for video in listOfFiles]
-
 # list = listOfVideoClips('Clips')
 # print()
 # print(list)
